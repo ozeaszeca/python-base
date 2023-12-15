0741a41c6625557ae48a4defd1ca7f30c2426753
Ozeas Montenegro <ozeasmslima@gmail.com>
2023-12-15 00:51:32 -0400

    primeiro programa hello world part1

---
+++ 03_hello.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Hello World Multi Línguas.
+
+Dependendo da lingua configurada no ambienteo programa exibe a mensagem correspondente.
+
+Como usar:
+
+verificar a linguagem do computador no terminal:
+    ➜ env | grep LANG
+    LANG=en_US.UTF-8
+    LANGUAGE=en_US
+
+Tenha a variável LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+
+    python3 03_hello.py
+    ou
+    ./03_hello.py
+
+
+"""
+__version__ = "0.0.1"
+__author__ = "Ozeas Montenegro"
+__license__ = "Unlicense"
+# Dundet __
+
+#current_language = "en_US"
+current_language = "it_IT"
+# snake case padrão comum do python
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
+
