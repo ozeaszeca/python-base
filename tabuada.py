018f063c48eb0e62c95d6b86ac5171bae4a14f05
Ozeas Montenegro <ozeasmslima@gmail.com>
2024-02-24 00:26:50 -0400

    atualizando o script da tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 """Imprime a tabuada do 1 ao 10.
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
 ...
---------------
-Tabuda do 2
-2
-4
+##################
+---Tabuda do 2---
+
+    2 x 1 = 2
+    2 x 2 = 4
 ...
--------------
+##################
 
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Ozeas"
 
 #base = [1,2,3,4,5,6,7,8,9,10]
@@ -21,14 +23,16 @@ numeros = list(range(1,11))
 
 # Iterable (percorriveis)
 
+template = ""
+
 #para cada numero em numeros:
 for numero in numeros:
-    print(f"Tabuada do :{numero}")
+    template += "{:-^19}".format(f"Tabuada do {numero}") + '\n\n'
+    
     for outro_numero in numeros:
-        print(f'{numero} x {outro_numero} = ',numero * outro_numero)
-    print('-----------------')
+        template += "{:^18}".format(f"{numero} x {outro_numero} = {numero * outro_numero}") + '\n'
+        
+    template += f'{19  * "#"} \n'
 
 
-
-
-
+print(template)
